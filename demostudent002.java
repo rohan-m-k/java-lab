1e7487c0cc39d822385a195325a4f070de723885
Rohan katkar <160541304+rohan-m-k@users.noreply.github.com>
2024-03-15 00:25:35 +0530

    Update demostudent002.java

--- demostudent002.java
+++ demostudent002.java
@@ -26,9 +26,9 @@ System.out.println("phone is ="+phone);
 class demostudent002{
 public static void main(String [] args)
 {
-student s1=new student("Sagar","2ba22cs074","cse",91,91838921);
-student s2=new student("Sharan","2ba22cs0800","cse",95,636578879);
-student s3=new student("Raju","2ba22cs03340","ec",94,745478879);
+student s1=new student("Rohan","2ba22cs070","cse",91,91838921);
+student s2=new student("Rohit","2ba22cs0071","cse",95,636578879);
+student s3=new student("Ram","2ba22cs03340","ec",94,745478879);
 s1.printdata();
 s2.printdata();
 s3.printdata();
